f3bc8142a49e3312ac5f7db2e5d70890cc728229
Serge Y. Kuprin <ga1nsboro@keyboardband.com>
2020-01-13 11:30:29 +0300

    NOM-4 #time 2h Выгрузка данных

---
+++ src/app/Console/Commands/Unloading/File.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Console\Commands\Unloading;
+
+use App\Models\Dosage\Form;
+use App\Models\Generic;
+use App\Models\Preparation;
+use App\Models\Product\ProductClass;
+use App\Models\Unit;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class File extends Command
+{
+    protected $signature = 'unloading:file';
+
+    protected $description = "File unloading.";
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $disk = Storage::disk('data');
+
+
+        if (($handle = fopen($disk->path('medicaments.csv'), 'r')) !== false) {
+            $i = 0;
+            while (($row = fgetcsv($handle, 1000, ",")) !== false) {
+                echo "{$i}\n";
+                $row = array_map('trim', $row);
+
+                $preparation = Preparation::firstOrCreate([
+                    'standard' => $row[0]
+                ]);
+
+                $unit = null;
+
+                if (!empty($row[6])) {
+                    $unit = Unit::firstOrCreate([
+                        'name' => $row[6]
+                    ]);
+                }
+
+                $productClass = ProductClass::firstOrCreate([
+                    'name' => $row[10]
+                ]);
+
+                $generic = Generic::firstOrCreate([
+                    'name' => $row[11]
+                ]);
+
+                $form = Form::firstOrCreate([
+                    'name' => $row[13]
+                ]);
+
+                $preparation->update([
+                    'name'             => $row[1],
+                    'dosage_short'     => $row[2],
+                    'packing'          => $row[3],
+                    'addition'         => empty($row[4]) ? null : $row[4],
+                    'volume'           => empty($row[5]) ? null : (double) $row[5],
+                    'unit_id'          => $unit ? $unit->id : null,
+                    'integer'          => $row[9],
+                    'product_class_id' => $productClass->id,
+                    'generic_name_id'  => $generic->id,
+                    'dosage'           => $row[12],
+                    'dosage_form_id'   => $form->id
+                ]);
+
+                $i++;
+            }
+            fclose($handle);
+        }
+
+        $this->info('File unloading completed successfully.');
+    }
+}
--- src/database/migrations/2020_01_10_110914_create_preparations_table.php
+++ src/database/migrations/2020_01_10_110914_create_preparations_table.php
@@ -16,22 +16,22 @@ class CreatePreparationsTable extends Migration
         Schema::create('preparations', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('standard')->comment('Эталонное наименование');
-            $table->string('name')->comment('Имя');
-            $table->string('dosage_short')->comment('Дозировка краткая / за 1 единицу');
-            $table->integer('packing')->comment('Фасовка');
+            $table->string('name')->nullable()->comment('Имя');
+            $table->string('dosage_short')->nullable()->comment('Дозировка краткая / за 1 единицу');
+            $table->integer('packing')->nullable()->comment('Фасовка');
             $table->string('addition')->nullable()->comment('Дополнение после фасовки');
             $table->double('volume')->nullable()->comment('Объем, вес и дозы');
             $table->unsignedBigInteger('unit_id')->nullable()->unsigned()->comment('Идентификатор единицы измерения');
             $table->foreign('unit_id')->references('id')->on('units')
                 ->onUpdate('cascade')->onDelete('set null');
-            $table->integer('quantity')->comment('Количество единиц препарата с учетом фасовки');
+            $table->integer('quantity')->nullable()->comment('Количество единиц препарата с учетом фасовки');
             $table->unsignedBigInteger('product_class_id')->nullable()->unsigned()->comment('Идентификатор класса товара');
             $table->foreign('product_class_id')->references('id')->on('product_classes')
                 ->onUpdate('cascade')->onDelete('set null');
             $table->unsignedBigInteger('generic_name_id')->nullable()->unsigned()->comment('Идентификатор международного непатентованного наименования');
             $table->foreign('generic_name_id')->references('id')->on('generic_names')
                 ->onUpdate('cascade')->onDelete('set null');
-            $table->string('dosage')->comment('Дозировка');
+            $table->string('dosage')->nullable()->comment('Дозировка');
             $table->unsignedBigInteger('dosage_form_id')->nullable()->unsigned()->comment('Идентификатор лекарственной формы');
             $table->foreign('dosage_form_id')->references('id')->on('dosage_forms')
                 ->onUpdate('cascade')->onDelete('set null');
