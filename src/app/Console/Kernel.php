52858bbcf62aba50bd7033d65a0df3061f1c6861
Serge Y. Kuprin <ga1nsboro@keyboardband.com>
2020-02-26 14:48:49 +0300

    NOM-4 #time 1h Cache

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -7,6 +7,7 @@ use App\Models\Item;
 use App\Models\Matching\Task;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
@@ -44,15 +45,21 @@ class Kernel extends ConsoleKernel
                             $categories = Category::with('attributes')->get();
 
                             while (($row = fgetcsv($handle, 1000, ";")) !== false) {
+                                $cache = false;
                                 $id = null;
                                 $highlight = [];
 
                                 $search = trim($row[1]);
 
-                                $hits = Item::search(['search' => $search, 'categories' => $categories])->raw()['hits']['hits'];
-                                if (count($hits)) {
-                                    $id = $hits[0]['_source']['id'];
-                                    $highlight = $hits[0]['highlight'];
+                                if (Cache::has(hash('sha512', $search))) {
+                                    $id = Cache::get(hash('sha512', $search));
+                                    $cache = true;
+                                } else {
+                                    $hits = Item::search(['search' => $search, 'categories' => $categories])->raw()['hits']['hits'];
+                                    if (count($hits)) {
+                                        $id = $hits[0]['_source']['id'];
+                                        $highlight = $hits[0]['highlight'];
+                                    }
                                 }
 
                                 $item = Item::where(['id' => $id])->with(['values' => function ($query) {
@@ -69,45 +76,49 @@ class Kernel extends ConsoleKernel
                                 $coincidence = false;
 
                                 if ($item) {
-                                    $result = "";
+                                    if ($cache) {
+                                        $coincidence = true;
+                                    } else {
+                                        $result = "";
 
-                                    $priority = false;
+                                        $priority = false;
 
-                                    foreach ($item->values as $value) {
-                                        if ($value->attribute->priority) {
-                                            if (strripos(mb_strtolower($search), mb_strtolower(trim($value->value))) !== false) {
-                                                $priority = true;
+                                        foreach ($item->values as $value) {
+                                            if ($value->attribute->priority) {
+                                                if (strripos(mb_strtolower($search), mb_strtolower(trim($value->value))) !== false) {
+                                                    $priority = true;
+                                                }
                                             }
+
+                                            if (!empty($fullStringResult)) {
+                                                $result .= " ";
+                                            }
+
+                                            $result .= (string)$value->value;
                                         }
 
-                                        if (!empty($fullStringResult)) {
-                                            $result .= " ";
+                                        if ($priority) {
+                                            $concurrencyAllAttribute = 0;
+
+                                            $match = [];
+
+                                            foreach ($highlight as $attr => $hit) {
+                                                $m = [];
+
+                                                preg_match_all('/<em[^>]*?>(.*?)<\/em>/si', array_shift($hit), $m);
+
+                                                $match = array_merge($match, $m[1]);
+                                            }
+
+                                            $match = array_unique($match);
+
+                                            foreach ($match as $term) {
+                                                $concurrencyAllAttribute += mb_strlen($term, "utf-8");
+                                            }
+
+                                            $coincidence = ((round((($concurrencyAllAttribute / mb_strlen(str_replace(" ", "", $result), "utf-8")) * 100), 1) >= 70) and
+                                                (round((($concurrencyAllAttribute / mb_strlen(str_replace(" ", "", $search), "utf-8")) * 100), 1) >= 70));
                                         }
-
-                                        $result .= (string) $value->value;
-                                    }
-
-                                    if ($priority) {
-                                        $concurrencyAllAttribute = 0;
-
-                                        $match = [];
-
-                                        foreach ($highlight as $attr => $hit) {
-                                            $m = [];
-
-                                            preg_match_all('/<em[^>]*?>(.*?)<\/em>/si', array_shift($hit), $m);
-
-                                            $match = array_merge($match, $m[1]);
-                                        }
-
-                                        $match = array_unique($match);
-
-                                        foreach ($match as $term) {
-                                            $concurrencyAllAttribute += mb_strlen($term, "utf-8");
-                                        }
-
-                                        $coincidence = ((round((($concurrencyAllAttribute / mb_strlen(str_replace(" ", "", $result), "utf-8")) * 100), 1) >= 70) and
-                                        (round((($concurrencyAllAttribute / mb_strlen(str_replace(" ", "", $search), "utf-8")) * 100), 1) >= 70));
                                     }
                                 }
 
@@ -129,6 +140,8 @@ class Kernel extends ConsoleKernel
                                     $items = $items->sortBy(function($item) use ($sequence) {
                                         return array_search($item->getKey(), $sequence);
                                     });
+                                } else {
+                                    Cache::put(hash('sha512', $search), $item->id, 20160);
                                 }
 
                                 $disk->append("result/{$task->id}.json", json_encode([
